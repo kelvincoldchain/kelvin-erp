c22a0dd358f3fd050ff97329bf741d0150c1bf30
Kelvin <kelvingithub@gmail.com>
2016-02-24 08:34:29 +0530

    First commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->library('Mongo_db', array('activate'=>'default'),'mongo_db2');
+		$a = ['hello'=>'fgf'];
+		//$a = json_encode($a);
+		var_dump($this->mongo_db2->where(array('key'=>'dgfd'))->get('users'));
+		$this->load->view('welcome_message');
+	}
+}
